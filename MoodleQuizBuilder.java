6cb1a97af8eb545f5d3548ae82a71ce0968c6d55
conor <conorclancy99@gmail.com>
2012-12-06 15:27:48 +0000

    True/False & Multiple Choice & Matching GUI

--- MoodleQuizBuilder.java
+++ MoodleQuizBuilder.java
@@ -16,6 +16,8 @@ import javax.swing.JRadioButton;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JSpinner;
+import java.awt.SystemColor;
+import java.awt.Color;
 
 
 public class MoodleQuizBuilder extends JFrame {
@@ -25,6 +27,15 @@ public class MoodleQuizBuilder extends JFrame {
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_9;
+	private JTextField textField_11;
+	private JTextField textField_8;
+	private JTextField textField_10;
+	private JTextField textField_12;
 
 	/**
 	 * Launch the application.
@@ -47,12 +58,13 @@ public class MoodleQuizBuilder extends JFrame {
 	 */
 	public MoodleQuizBuilder() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 665, 456);
+		setBounds(100, 100, 628, 456);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		JPanel panel1 = new JPanel();
+		panel1.setBackground(new Color(220, 220, 220));
 		
 
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
@@ -60,7 +72,7 @@ public class MoodleQuizBuilder extends JFrame {
 		panel1.setLayout(new MigLayout("", "[grow 20][grow][grow 20]", "[][grow][][][][][]"));
 		
 		JLabel lblTitleoptional = new JLabel("Title (Optional)");
-		panel1.add(lblTitleoptional, "flowx,cell 1 0");
+		panel1.add(lblTitleoptional, "cell 0 0,alignx center");
 		
 		textField = new JTextField();
 		panel1.add(textField, "cell 1 0,growx");
@@ -98,11 +110,12 @@ public class MoodleQuizBuilder extends JFrame {
 		contentPane.add(tabbedPane, BorderLayout.CENTER);
 		
 		JPanel panel = new JPanel();
+		panel.setBackground(new Color(220, 220, 220));
 		tabbedPane.addTab("Multiple Choice", null, panel, null);
-		panel.setLayout(new MigLayout("", "[grow 20][grow][grow 20]", "[][grow][][][][][]"));
+		panel.setLayout(new MigLayout("", "[grow 20][grow][grow 20]", "[][grow][][][][][][][]"));
 		
 		JLabel lblTitleoptional_1 = new JLabel("Title (Optional)");
-		panel.add(lblTitleoptional_1, "flowx,cell 1 0");
+		panel.add(lblTitleoptional_1, "cell 0 0,alignx center");
 		
 		textField_1 = new JTextField();
 		panel.add(textField_1, "cell 1 0,growx");
@@ -113,9 +126,6 @@ public class MoodleQuizBuilder extends JFrame {
 		
 		JTextArea textArea_1 = new JTextArea();
 		panel.add(textArea_1, "cell 1 1,grow");
-		
-		JButton btnAddAnswer = new JButton("Add Answer");
-		panel.add(btnAddAnswer, "flowx,cell 1 2,alignx right");
 		
 		JButton btnClearText_1 = new JButton("Clear Text");
 		panel.add(btnClearText_1, "cell 1 2,alignx right");
@@ -134,14 +144,88 @@ public class MoodleQuizBuilder extends JFrame {
 		JSpinner spinner_1 = new JSpinner();
 		panel.add(spinner_1, "cell 2 4,alignx center");
 		
+		textField_4 = new JTextField();
+		panel.add(textField_4, "cell 1 5,growx");
+		textField_4.setColumns(10);
+		
+		JSpinner spinner_2 = new JSpinner();
+		panel.add(spinner_2, "cell 2 5,alignx center");
+		
+		textField_5 = new JTextField();
+		panel.add(textField_5, "cell 1 6,growx");
+		textField_5.setColumns(10);
+		
+		JSpinner spinner_3 = new JSpinner();
+		panel.add(spinner_3, "cell 2 6,alignx center");
+		
 		JButton btnSave_1 = new JButton("Save");
-		panel.add(btnSave_1, "cell 1 5");
+		panel.add(btnSave_1, "cell 1 7");
 		
 		JButton btnNewButton_1 = new JButton("Add another Multiple Choice");
-		panel.add(btnNewButton_1, "cell 1 6");
+		panel.add(btnNewButton_1, "cell 1 8");
 		
 		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(220, 220, 220));
 		tabbedPane.addTab("Matching", null, panel_1, null);
+		panel_1.setLayout(new MigLayout("", "[grow 10][grow 30][grow 50][grow 10]", "[fill][][][][][][][][][][][]"));
+		
+		JLabel lblTitleoptional_2 = new JLabel("Title (optional)");
+		panel_1.add(lblTitleoptional_2, "cell 0 0,alignx center");
+		
+		textField_6 = new JTextField();
+		panel_1.add(textField_6, "cell 1 0 2 1,growx");
+		textField_6.setColumns(10);
+		
+		JLabel lblA = new JLabel("A");
+		panel_1.add(lblA, "flowx,cell 1 2,alignx trailing");
+		
+		textField_7 = new JTextField();
+		panel_1.add(textField_7, "cell 1 2,growx");
+		textField_7.setColumns(10);
+		
+		JLabel lblA_1 = new JLabel("A");
+		panel_1.add(lblA_1, "flowx,cell 2 2");
+		
+		textField_8 = new JTextField();
+		panel_1.add(textField_8, "cell 2 2,growx");
+		textField_8.setColumns(10);
+		
+		JLabel lblB = new JLabel("B");
+		panel_1.add(lblB, "flowx,cell 1 4,alignx trailing");
+		
+		textField_9 = new JTextField();
+		panel_1.add(textField_9, "cell 1 4,growx");
+		textField_9.setColumns(10);
+		
+		JLabel lblB_1 = new JLabel("B");
+		panel_1.add(lblB_1, "flowx,cell 2 4");
+		
+		textField_10 = new JTextField();
+		panel_1.add(textField_10, "cell 2 4,growx");
+		textField_10.setColumns(10);
+		
+		JLabel lblC = new JLabel("C");
+		panel_1.add(lblC, "flowx,cell 1 6,alignx trailing");
+		
+		textField_11 = new JTextField();
+		panel_1.add(textField_11, "cell 1 6,growx");
+		textField_11.setColumns(10);
+		
+		JLabel lblC_1 = new JLabel("C");
+		panel_1.add(lblC_1, "flowx,cell 2 6");
+		
+		textField_12 = new JTextField();
+		panel_1.add(textField_12, "cell 2 6,growx");
+		textField_12.setColumns(10);
+		
+		JButton btnClear = new JButton("Clear Text");
+		panel_1.add(btnClear, "cell 2 8,alignx right");
+		
+		JButton btnSave_2 = new JButton("Save");
+		panel_1.add(btnSave_2, "cell 1 10");
+		
+		JButton btnAddAnotherMatching = new JButton("Add another Matching");
+		panel_1.add(btnAddAnotherMatching, "cell 1 11");
 		
 		JPanel panel_2 = new JPanel();
 		tabbedPane.addTab("New tab", null, panel_2, null);
